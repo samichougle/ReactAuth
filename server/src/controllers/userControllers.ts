f35758d443d39e891b7007a78f5252f9f2b0ff96
samichougle <samichougle.saac@gmail.com>
2025-11-18 11:51:48 +0530

    Fix email verification link in userControllers by correcting the URL format to ensure proper functionality.

--- server/src/controllers/userControllers.ts
+++ server/src/controllers/userControllers.ts
@@ -113,7 +113,7 @@ export const sendVerificationMail: RequestHandler = async (req, res, next) => {
           <p>Click the button below to verify your email:</p>
 
           <div style="text-align:center; margin:25px 0;">
-            <a href="https://https://samichougle-react-auth-drab.vercel.app/user/email-verify/${jwtToken}"
+            <a href="https://https:samichougle-react-auth-drab.vercel.app/user/email-verify/${jwtToken}"
               style="background:#4f46e5; color:white; padding:12px 22px; border-radius:6px; text-decoration:none;">
               Verify Email
             </a>  
